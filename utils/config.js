24b76dcd0deb69776e4864b494827ae7428881a0
Denis AKPAGNONITE <akpagnonited@gmail.com>
2022-11-14 09:07:47 +0100

    correction du bug de process env

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,5 @@
 const config = {
-    baseUrl: process.env.BASE_URL || 'http://localhost:3000',
-    inProduction: process.env.IN_PRODUCTION || false,
+    baseUrl: "https://denisakp.me",
     site: {
         name: "Denis AKPAGNONITE",
         description: "La description de mon site web",
