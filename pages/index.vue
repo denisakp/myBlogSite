c54b727b9e09e3d6c504e40db6bbf896a09417fc
Denis AKPAGNONITE <akpagnonited@gmail.com>
2022-11-15 08:18:57 +0100

    Refactoring the meta seao

--- pages/index.vue
+++ pages/index.vue
@@ -11,15 +11,14 @@ const query = { limit: 7, sort: { date: -1 }, only: ['title', 'description', 'ta
 <template>
   <div>
     <Head>
-      <Title>Accueil</Title>
-      <Meta name="title" content="Accueil" />
+      <Meta name="title" content="DenisAKP — Tutos en DevOPS, Cloud Infrastructure & Ingénierie logicielle" />
       <Meta name="description" content="Retrouvez gratuitement des articles et tutoriels portant sur les thématique du DevOPS, le cloud Infrastructure & l'ingénierie logicielle." />
       <Meta name="keywords" content="denisakp, devops, ingénierie logicielle, tutos" />
       <Meta name="og:description" content="Retrouvez gratuitement des articles et tutoriels portant sur les thématique du DevOPS, le cloud Infrastructure & l'ingénierie logicielle." />
-      <Meta name="og:title" content="Accueil" />
+      <Meta name="og:title" content="DenisAKP — Tutos en DevOPS, Cloud Infrastructure & Ingénierie logicielle" />
       <Meta name="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
       <Meta name="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
-      <Meta name="twitter:tile" content="Accueil" />
+      <Meta name="twitter:tile" content="DenisAKP — Tutos en DevOPS, Cloud Infrastructure & Ingénierie logicielle" />
       <Meta name="twitter:description" content="Retrouvez gratuitement des articles et tutoriels portant sur les thématique du DevOPS, le cloud Infrastructure & l'ingénierie logicielle." />
     </Head>
     <div class="page-bg">
