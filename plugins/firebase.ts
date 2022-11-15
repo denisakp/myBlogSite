e8164a1f8af741fab2a61c900950923ff8b6453e
Denis AKPAGNONITE <akpagnonited@gmail.com>
2022-11-15 03:50:35 +0100

    Config firebase

---
+++ plugins/firebase.ts
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {useState} from "#app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB8F9UWoeDjWsAhVNpwGAi4KXr8Q84uqqA",
+    authDomain: "my-website-faddb.firebaseapp.com",
+    projectId: "my-website-faddb",
+    storageBucket: "my-website-faddb.appspot.com",
+    messagingSenderId: "1060227957129",
+    appId: "1:1060227957129:web:94d51cbbef57f4a36a2158",
+    measurementId: "G-G4YK1FGCB5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
+export default defineNuxtPlugin(nuxtApp => {
+    useState('firebaseApp', () => app)
+    useState('analytics', () => analytics)
+})
