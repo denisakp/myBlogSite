b74b20ab009ed6c9db33b3cd82372d0ef5e8c38e
Denis AKPAGNONITE <akpagnonited@gmail.com>
2023-01-09 19:34:32 +0100

    added github link

--- data/project.js
+++ data/project.js
@@ -1,10 +1,11 @@
 const projects = [
         {
             "title": "DenisAKP (Site web)",
-            "description": "Site web où je partage des articles et tutos sur les thématiques du DevOPS, le cloud Infrastructure & d'ingénierie logicielle. ",
+            "description": "Site web où je partage des articles et tutos sur les thématiques du DevOPS, du cloud Infrastructure & de la sécurité des applications. ",
             "date": "Novembre 2022",
-            "tags": ["nuxt3", "nuxt-content", "vercel"],
-            "preview": "https://denisakp.me"
+            "tags": ["nuxt3", "nuxt-content", "digitalocean"],
+            "preview": "https://denisakp.me",
+            "github": "https://github.com/denisakp/myBlogSite"
         }
     ];
 export default projects;
